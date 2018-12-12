0d7e0cf0ffeb6c488fcc599ca1c0f03480ee9825
Aditi Haiman <aditi@aditis-air.lan>
2018-12-12 17:50:49 -0500

    add and toString

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,8 +1,21 @@
 public class MyLinkedList {
-  private MyNode start;
-  private MyNode end;
+  private MyNode end, start, current;
   private int size;
 
-  
+  public int size() {
+    return size;
+  }
+
+  public boolean add(int value) {
+    end.setNext(new MyNode(value));
+    return true;
+  }
+
+  public String toString() {
+    String output = "";
+
+    return output;
+  }
+
 
 }
