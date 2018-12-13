407dbf4f7b5dd1a13270184d0f8edf9a7376b4e4
Aditi Haiman <aditi@Aditis-MacBook-Air.local>
2018-12-13 10:52:38 -0500

    testing methods

--- Driver.java
+++ Driver.java
@@ -7,7 +7,13 @@ public class Driver {
       list.add(x);
     }
 
+    System.out.println(list); // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    System.out.println(list.get(4)); //should print 4
+    System.out.println(list.contains(8)); //true
+    System.out.println(list.contains(-1)); //false
+    System.out.println(list.set(0, -1)); // should print 0
     System.out.println(list);
-
   }
+
+
 }
