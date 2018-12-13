6a6d33711e3333e2b1c7b9cf138e83c6a9732f4c
Aditi Haiman <aditi@Aditis-MacBook-Air.local>
2018-12-13 10:02:55 -0500

    worked on add(value)

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,24 +1,45 @@
 public class MyLinkedList {
-  private MyNode end, start, current;
-  private int size;
+  private MyNode end, start;
+  private int length;
 
-  public int size() {
-    return size;
+
+  public MyLinkedList() {
+    length = 0;
+    start = new MyNode(null);
+    end = new MyNode(null);
   }
 
-  public boolean add(int value) {
-    end.setNext(new MyNode(value));
+
+  public int size() {
+    return length;
+  }
+
+  public boolean add(Integer value) {
+    MyNode newNode = new MyNode(value);
+    if (length == 0) {
+      start.setNext(newNode);
+      newNode.setNext(end);
+      newNode.setPrev(start);
+      end.setPrev(newNode);
+    }
+    else{
+      newNode.setPrev(end.prev());
+      end.prev().setNext(newNode);
+      end.setPrev(newNode);
+      newNode.setNext(end);
+    }
+    length++;
     return true;
   }
 
   public String toString() {
-    current = start.getNext();
-    String output = "[" + start.getData();
-    while (current != end) {
-      output += ", " + current.getData();
-      current = current.getNext();
-    }
-    return output + end.getData();
+    // current = start.next();
+    // String output = "[" + start.getData();
+    // while (current != end) {
+    //   output += ", " + current.getData();
+    //   current = current.getNext();
+    // }
+    // return output + end.getData();
   }
 
 
