22061fda8d80b20c2f3064c521d1dc803167ec14
Aditi Haiman <aditi@Aditis-MacBook-Air.local>
2018-12-14 12:17:24 -0500

    added more tests

--- Driver.java
+++ Driver.java
@@ -47,7 +47,45 @@ public class Driver {
     System.out.println();
     System.out.println("---- Testing get(index) ----");
     System.out.println();
-    
+    list = new MyLinkedList();
+    for (int x = 0; x < 10; x++){
+      list.add(x);
+    }
+    System.out.println("Current list: " + list);
+    System.out.println("Get first index should print 1: " + list.get(1));
+    System.out.print("Get -1th index, should print an error: ");
+    try {
+      list.get(-1);
+    }
+    catch (IndexOutOfBoundsException e) {
+      System.out.println(e);
+    }
+    System.out.println();
+    System.out.println();
+    System.out.println("---- Testing set(index, value) ----");
+    System.out.println();
+    System.out.println("Current list: " + list);
+    System.out.println("Set 3rd index to 10 should print 3: " + list.set(3, 10));
+    System.out.println("Current list: " + list);
+    System.out.print("Set -1th index, should print an error: ");
+    try {
+      list.set(-1, 0);
+    }
+    catch (IndexOutOfBoundsException e) {
+      System.out.println(e);
+    }
+    for (int x = 0; x < 10; x++) {
+      list.set(x, x+1);
+    }
+    System.out.println("Setting every index of list with a loop, should be a list of 1-10 inclusive: " + list);
+    System.out.println();
+    System.out.println();
+    System.out.println("---- Testing contains and indexOf ----");
+    System.out.println("list.contains(6) should print true: " + list.contains(6));
+    System.out.println("list.contains(70) should print false: " + list.contains(70));
+    System.out.println("list.indexOf(4) should print 3: " + list.indexOf(4));
+    System.out.println("list.indexOf(50) should print -1: " + list.indexOf(50));
+
 
 
 
