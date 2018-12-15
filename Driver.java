2f6ab48a11c1bf91473b535af373c15d2b84a77f
Aditi Haiman <aditi@aditis-air.lan>
2018-12-14 20:48:00 -0500

    added more tests

--- Driver.java
+++ Driver.java
@@ -1,28 +1,6 @@
 public class Driver {
   public static void main(String[] args) {
 
-    // MyLinkedList list = new MyLinkedList();
-    // System.out.println(list);
-    // for (int x = 0; x < 10; x++) {
-    //   list.add(x);
-    // }
-    // Integer num = 9;
-    // System.out.println(list); // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    // System.out.println(list.get(4)); //should print 4
-    // System.out.println(list.contains(8)); //true
-    // System.out.println(list.contains(-1)); //false
-    // System.out.println(list.set(0, -1)); // should print 0
-    // System.out.println(list);
-    // System.out.println("Removing 9th index: " + list.remove(9)); // should print 9
-    // System.out.println(list);
-    // list.add(9);
-    // System.out.println();
-    // System.out.println("Resetting list: " + list);
-    // System.out.println();
-    // System.out.println("Removing number 9: " + list.remove(num)); //should print true
-    // System.out.println(list);
-
-
     System.out.println("---------TESTING MYLINKEDLIST ---------");
     System.out.println();
 
@@ -62,6 +40,7 @@ public class Driver {
     }
     System.out.println();
     System.out.println();
+
     System.out.println("---- Testing set(index, value) ----");
     System.out.println();
     System.out.println("Current list: " + list);
@@ -80,17 +59,54 @@ public class Driver {
     System.out.println("Setting every index of list with a loop, should be a list of 1-10 inclusive: " + list);
     System.out.println();
     System.out.println();
+
     System.out.println("---- Testing contains and indexOf ----");
+    System.out.println();
     System.out.println("list.contains(6) should print true: " + list.contains(6));
     System.out.println("list.contains(70) should print false: " + list.contains(70));
     System.out.println("list.indexOf(4) should print 3: " + list.indexOf(4));
     System.out.println("list.indexOf(50) should print -1: " + list.indexOf(50));
+    System.out.println();
+    System.out.println();
 
+    System.out.println("---- Testing add(index, value) ----");
+    System.out.println();
+    System.out.println("Current list: " + list);
+    System.out.print("Add to -1th index, should print an error: ");
+    try {
+      list.add(-1, 0);
+    }
+    catch (IndexOutOfBoundsException e) {
+      System.out.println(e);
+    }
+    list.add(0, 0);
+    System.out.println("Add 0 to the 0th index: " + list);
+    list.add(10, 11);
+    System.out.println("Add 11 to the 10th index: " + list);
+    System.out.println();
+    System.out.println();
 
-
-
-
-
+    System.out.println("---- Testing remove methods ----");
+    System.out.println();
+    System.out.println("Current list: " + list);
+    System.out.println("Remove 0th index should print 0: " + list.remove(0));
+    System.out.println("Current list: " + list);
+    System.out.print("Removing -1th index, should print an error: ");
+    try {
+      list.remove(-1);
+    }
+    catch (IndexOutOfBoundsException e) {
+      System.out.println(e);
+    }
+    Integer num = 11;
+    System.out.println("Removing by value, trying to remove 11, should print true: " + list.remove(num));
+    System.out.println("Current list: " + list);
+    num = 100;
+    System.out.println("Trying to remove value 100, should print false: " + list.remove(num));
+    System.out.println("Current list: " + list);
+    System.out.println();
+    System.out.println();
+    System.out.println("--------- END OF TEST ----------");
 
   }
 
